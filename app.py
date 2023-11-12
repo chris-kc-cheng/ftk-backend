883acae7c2b9596e80535eaa3d029fa412c77b7a
chris-kc-cheng <kc1116@gmail.com>
2023-11-12 09:12:09 -0500

    Added routes

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+import toolkit as ftk
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index():
     return "<p>Financial Toolkit</p>"
+
+@app.route('/ticker/<ticker>')
+def get_price(ticker):
+    return ftk.get_yahoo(ticker)
