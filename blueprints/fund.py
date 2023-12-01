157aa278d72b851131c83dd43845ae1c0f18ccb9
chris-kc-cheng <kc1116@gmail.com>
2023-12-01 07:55:26 -0500

    Fund Creation

--- blueprints/fund.py
+++ blueprints/fund.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 import flask_praetorian
-from extensions import db
+from mongoengine.errors import NotUniqueError
 from models.fund import Fund
 
 bp_fund = Blueprint('fund', __name__)
@@ -16,10 +16,52 @@ def add():
 @flask_praetorian.auth_required
 def create():
     req = request.get_json(force=True)
-    name = req.get('fundName', None)    
-    return jsonify({'sum': 123})
+    print('Creating', req)
+    # Frontend sends empty string for name and None for launchDate and empty array for assetClasses
+    fundName = req.get('fundName', '')
+    firmName = req.get('firmName', '')
+    assetClasses = req.get('assetClasses', [])
+    launchDate = req.get('launchDate', None)
+    if len(fundName) == 0:
+        return jsonify({
+            'status': 'error',
+            'message': 'Fund name cannot be empty'})
+    if len(firmName) == 0:
+        return jsonify({
+            'status': 'error',
+            'message': 'Firm name cannot be empty'})
+    if len(assetClasses) == 0:
+        return jsonify({
+            'status': 'error',
+            'message': 'At least one asset class must be specified'})
+    fund = Fund(name=fundName, firm=firmName, assetClasses=assetClasses, launchDate=launchDate)
+    try:
+        fund.save()
+    except NotUniqueError:
+        return jsonify({
+            'status': 'error',
+            'message': 'Fund name is duplicated'})
+    return jsonify({
+        'status': 'success',
+        'message': f'{fundName} is created',
+        'id': str(fund.id)
+    })
 
 @bp_fund.route('/all')
 @flask_praetorian.auth_required
 def all():
+    """Get list of all funds for autocomplete
+    """
     return jsonify(Fund.objects.only('name').order_by('name')), 200
+
+@bp_fund.route('/detail/<id>')
+@flask_praetorian.auth_required
+def detail(id):
+    fund = Fund.objects.get(pk=id)
+    ret = {
+        'name': fund.name,
+        'firm': fund.firm,
+        'assetClasses': fund.assetClasses,
+        'launchDate': fund.launchDate
+    }
+    return jsonify(ret), 200
--- models/fund.py
+++ models/fund.py
@@ -3,6 +3,6 @@ from mongoengine import Document, fields
 class Fund(Document):
     name = fields.StringField(required=True, unique=True)
     firm = fields.StringField(required=True)
-    assetClasses = fields.ListField(fields.StringField())
-    #portfolioManagers = fields.ListField(fields.StringField())
+    assetClasses = fields.ListField(fields.StringField())    
     launchDate = fields.DateField()
+    #portfolioManagers = fields.ListField(fields.StringField())
